89dfe51f555c0ac6c431497152fc7dbc3b1efe0d
RocketMa <ma2014119@outlook.com>
2021-06-08 13:30:10 +0800

    11 Delete unused static variables, update stuInfo, enlarge MainWindow.fxml, created AutoIterator, EnhancedList, add buttons and textfields into grid pane, changed fxml field names, stop applying css to grid pane, update mslM(), create updateTable(), override equals() and toString() to Student,

--- src/cn/rocket/deksrt/main/Student.java
+++ src/cn/rocket/deksrt/main/Student.java
@@ -1,9 +1,24 @@
 package cn.rocket.deksrt.main;
 
 public class Student {
-    private String name;
-    private String pinyin;
-    private boolean boarding;
+    private final String name;
+    private final String pinyin;
+    private final boolean boarding;
+
+    @Override
+    public boolean equals(Object obj) {
+        Student stu;
+        if (obj instanceof Student)
+            stu = (Student) obj;
+        else
+            return false;
+        return name.equals(stu.name) && pinyin.equals(stu.pinyin) && boarding == stu.boarding;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + name + "," + pinyin + "," + boarding + ">";
+    }
 
     public Student(String name, String pinyin, boolean boarding) {
         this.name = name;
